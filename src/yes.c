c47b59bc6eaa36c068084492e4c89ab25e88ded1
Eduardo Francisco <eadwardus@disroot.org>
2019-03-16 15:58:53 -0300

    Add missing trailing newline

--- src/yes.c
+++ src/yes.c
@@ -6,7 +6,7 @@
 static void
 usage(void)
 {
-	c_ioq_fmt(ioq2, "usage: %s [expletive]", c_std_getprogname());
+	c_ioq_fmt(ioq2, "usage: %s [expletive]\n", c_std_getprogname());
 	c_ioq_flush(ioq2);
 	c_std_exit(1);
 }
