ccb8301511c760f9399fa6561929789726fa159c
Eduardo Francisco <eadwardus@disroot.org>
2021-09-21 17:33:52 -0300

    lib/common/path.c: Fix pathcat

--- lib/common/path.c
+++ lib/common/path.c
@@ -10,8 +10,9 @@ pathcat(char *s, char *d, int cat)
 	ctype_arr arr;
 	ctype_stat st;
 
-	c_arr_init(&arr, buf, sizeof(buf));
 	c_str_rtrim(d, -1, "/");
+	c_arr_init(&arr, buf, sizeof(buf));
+	c_arr_fmt(&arr, "%s", d);
 	if (!cat) {
 		if (c_nix_stat(&st, d) < 0 || !C_ISDIR(st.mode))
 			return c_arr_data(&arr);
